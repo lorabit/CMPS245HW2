c3f86e1fced9c36b15f3eaec09a2dda8749912be
Ziqiang Wang <ziqiang@think3wang.local>
2017-02-04 15:21:17 -0800

    modify topicmodel.py

--- topicmodel.py
+++ topicmodel.py
@@ -1,13 +1,37 @@
-import genism
-from nltk.tokenize import RegexpTokenizer
-
-texts = [];
-
-with open('preprocessed.csv','rb') as csvfile:
-	rows = csv_reader(csvfile)
-	for row in rows:
-		texts.append(row[0])
+import gensim
+from common import *
+from nltk.tokenize import TweetTokenizer
+from gensim import corpora, models
 
 
-for text in texts:
+
+
+
+
 	
+def lda_model(filename):
+
+	tknzr = TweetTokenizer()
+	texts_tokenized = []
+
+	with open(filename, 'rb') as csvfile:
+		rows = csv_reader(csvfile)
+		line = 0
+		for row in rows:
+			line += 1
+			if line == 1:
+				continue
+			texts_tokenized.append(tknzr.tokenize(row[0]))
+	
+	# print texts
+
+	dictionary = corpora.Dictionary(texts_tokenized)
+	corpus = [dictionary.doc2bow(text) for text in texts_tokenized]
+	ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=3, id2word = dictionary, passes=20)
+	print(ldamodel.print_topics(num_topics=3, num_words=3))
+	# print ldamodel
+	print(ldamodel.get_topic_terms(topicid = 0, topn=15))
+	
+
+if __name__ == '__main__':
+	lda_model('test_preprocessed.csv')
