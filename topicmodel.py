9ad89848cff78f3032549079204d9b9e13572b16
Ziqiang Wang <ziqiang@think3wang.local>
2017-02-03 22:45:04 -0800

    ziqiang make change on topicmodel.py

--- topicmodel.py
+++ topicmodel.py
@@ -1,7 +1,13 @@
 import genism
-
+from nltk.tokenize import RegexpTokenizer
 
 texts = [];
 
 with open('preprocessed.csv','rb') as csvfile:
 	rows = csv_reader(csvfile)
+	for row in rows:
+		texts.append(row[0])
+
+
+for text in texts:
+	
